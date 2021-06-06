956150f108dac5dd32e58f6b769361356d2161ec
Bella <67583659+honeylemonicetea@users.noreply.github.com>
2021-06-06 10:10:42 +0300

    Delete Drafts directory

--- Drafts/juz stuff/image test.py
+++
@@ -1,8 +0,0 @@
-# downloads an image with a specified url
-
-import requests
-
-resp  = requests.get('https://media.timeout.com/images/105731796/1024/576/image.jpg')
-
-with open('image.jpg', 'wb') as file2:
-    file2.write(resp.content)
